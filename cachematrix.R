09625aa7ba91afd07e5f91e4dff5c5e2816a5744
Rodrigo A. Arvolea <arvolea@hotmail.com>
2014-12-21 10:40:14 -0200

    Comment changed

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## Caching the Inverse of a Matrix
+## Coursera Assignment - Caching the Inverse of a Matrix
 
 ## Matrix inversion is usually a costly computation and there may be some benefit to caching the 
 ## inverse of a matrix rather than computing it repeatedly (there are also alternatives to matrix 
